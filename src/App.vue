5f90348248bcb6f3e4d7af62746878449ccb6441
LordAndras <sarrotanba@gmail.com>
2024-05-10 11:02:45 +0200

    feat(steps): add file upload view

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,17 @@
 import {editorSteps} from "./constants/steps.ts";
 import {ref} from "vue";
 import {cloneDeep} from "lodash-es";
-import FileUploader from "./components/FileUploader.vue";
+import GeolocationUpload from "./views/GeolocationUpload.vue";
 
 const steps = ref(cloneDeep(editorSteps))
-const isValidFile = ref(true)
 
 function goToStep(index: number) {
   steps.value.forEach((step) => {
     step.selected = index === step.index
   })
 }
+
+const csvError = ref(true)
 
 </script>
 
@@ -26,15 +27,7 @@ function goToStep(index: number) {
     </header>
     <main class="e-layout__content e-layout__content-form">
       <section v-if="steps[0].selected" class="e-layout__section e-padding-l">
-        <div>
-          <div class="e-section__title">Upload Geolocations CSV</div>
-        </div>
-        <div class="e-section__content">
-          <FileUploader :disabled="false" @file-upload-error="isValidFile = false"></FileUploader>
-        </div>
-        <span v-if="!isValidFile" class="e-field__message e-field__message-error">
-        <span>{{ 'This file is not a CSV.' }}</span>
-      </span>
+        <GeolocationUpload :is-valid="csvError" v-model="csvError"/>
       </section>
       <section v-if="steps[1].selected" class="e-layout__section e-padding-l">
         <div class="e-section__header">
@@ -57,7 +50,7 @@ function goToStep(index: number) {
         </div>
       </section>
       <e-step-bar floating>
-        <e-step v-for="step in steps" :key="step.toString()" :label="step.label" @trigger="goToStep(step.index)"
+        <e-step v-for="step in steps" :key="step.toString()" :label="step.label" :disabled="!csvError" @trigger="goToStep(step.index)"
                 :selected="`${step.selected}`"></e-step>
       </e-step-bar>
     </main>
---
+++ src/views/GeolocationUpload.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import FileUploader from "../components/FileUploader.vue";
+
+interface Props {
+  isValid: boolean
+}
+
+defineProps<Props>()
+
+const onUploadError = () => {
+  emit('update:model-value', false)
+}
+
+const emit = defineEmits<{
+  (event: 'update:model-value', value: boolean): void
+}>()
+
+</script>
+
+<template>
+  <div>
+    <div class="e-section__title">Upload Geolocations CSV</div>
+  </div>
+  <div class="e-section__content">
+    <FileUploader :disabled="false" @file-upload-error="onUploadError"></FileUploader>
+  </div>
+  <span v-if="!isValid" class="e-field__message e-field__message-error">
+        <span>{{ 'This file is not a CSV.' }}</span>
+      </span>
+</template>
+

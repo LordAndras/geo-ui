f42a60e9fa8f97b76d0fde0f7d8646713d36de1a
Andras Sarro <lordandras@users.noreply.github.com>
2024-05-31 12:32:08 +0200

    feat(location-list): change list to accordion

--- src/views/GeolocationList.vue
+++ src/views/GeolocationList.vue
@@ -1,108 +1,28 @@
 <script setup lang="ts">
+import {useGeoLocationsStore} from "../store/geo-locations-store.ts";
+import {GeoLocation} from "../lib/utils/geo-csv-parser.ts";
 
+const locationStore = useGeoLocationsStore()
+
+function isLocationValid(location: GeoLocation): boolean {
+  return locationStore.invalidLocations?.includes(location) ?? false
+}
 </script>
 
 <template>
-  <div style="width: 100vw; height: 50vh" class="e-scrollable">
+  <div style="width: 90%; height: 50vh" class="e-scrollable">
     <div class="e-scrollable__content">
-      <ul class="e-list e-list-bulleted">
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-        <li class="e-list__item">
-          Item 1
-        </li>
-        <li class="e-list__item">
-          Item 2
-        </li>
-        <li class="e-list__item">
-          Item 3
-        </li>
-        <li class="e-list__item">
-          Item 4
-        </li>
-      </ul>
+      <e-accordion>
+        <e-accordion-item v-for="location in locationStore.locations" :key="JSON.stringify(location)">
+          <e-accordion-item-label :class="{'text-color-error': isLocationValid(location)}">
+            {{ location.desc }}
+          </e-accordion-item-label>
+          <e-accordion-item-content :class="{'text-color-error': isLocationValid(location)}">
+            <p>Latitude: {{ location.lat }}</p>
+            <p>Longitude: {{ location.lon }}</p>
+          </e-accordion-item-content>
+        </e-accordion-item>
+      </e-accordion>
     </div>
   </div>
 </template>
